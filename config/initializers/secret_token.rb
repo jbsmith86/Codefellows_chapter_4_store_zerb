e6f414845ade5bb2cbe0d413cd0f507f64463296
Joel Smith <joely5525@gmail.com>
2014-01-16 15:50:08 -0800

    added zerb to store

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Chapter4StoreZerb::Application.config.secret_key_base = 'fe9e3aa0101c54f0f58d456c7e775017151e12724977243804faa9e2ab68acf150d4f50d28b4dbdd9ef582ee11778cc1dbad235c5c991232e57d48248e5fad6d'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Chapter4StoreZerb::Application.config.session_store :cookie_store, key: '_chapter_4_store_zerb_session'
